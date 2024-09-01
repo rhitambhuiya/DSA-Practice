f669233ae4a584e391b632aac1b81fb13eff1d1a
Rhitam Bhuiya <rhitambhuiya@yahoo.com>
2024-09-01 12:32:48 +0530

    Optimal Code

---
+++ Arrays/Rotated_Sorted.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check(vector<int>& nums) {
+    int n = nums.size();
+    int count = 0;
+    for(int i = 1; i< n; i++)
+        if(nums[i-1] > nums[i])
+            count++;
+    if(nums[n-1] > nums[0])
+        count++;
+    return count <= 1;
+}
+
+int main(){
+    vector<int> v;
+    int input;
+    while(cin>>input)
+        v.push_back(input);
+    if(check(v))
+        cout<<"Array is sorted after rotating";
+    else
+        cout<<"Array can't be sorted";
+    return 0;
+}
